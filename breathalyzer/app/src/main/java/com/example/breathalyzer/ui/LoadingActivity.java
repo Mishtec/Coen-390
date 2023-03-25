6ad82ac4f1f128886cca6823bc1a1e26b7afdb61
Amish <91218131+Mishtec@users.noreply.github.com>
2023-03-25 04:03:25 -0400

    Update LoadingActivity.java
    double back to exit

--- breathalyzer/app/src/main/java/com/example/breathalyzer/ui/LoadingActivity.java
+++ breathalyzer/app/src/main/java/com/example/breathalyzer/ui/LoadingActivity.java
@@ -2,8 +2,9 @@ package com.example.breathalyzer.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -11,12 +12,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.breathalyzer.MainActivity;
 import com.example.breathalyzer.R;
-import com.example.breathalyzer.ResultsActivity;
-import com.example.breathalyzer.data.DatabaseHandler;
-import com.example.breathalyzer.model.History;
-import com.example.breathalyzer.ui.ProgressBarAnimation;
 
 
 public class LoadingActivity extends AppCompatActivity {
@@ -24,6 +20,8 @@ public class LoadingActivity extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView, disclaimer;
     Animation discAnim;
+    boolean exit = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,20 +37,34 @@ public class LoadingActivity extends AppCompatActivity {
         progressBar.setScaleY(3f);
         progressAnimation();
 
-        discAnim = AnimationUtils.loadAnimation(this,R.anim.disclaimer_anim);
+        discAnim = AnimationUtils.loadAnimation(this, R.anim.disclaimer_anim);
         disclaimer.setAnimation(discAnim);
 
     }
-    public void progressAnimation(){
+
+    public void progressAnimation() {
         ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);
         anim.setDuration(10000);
         progressBar.setAnimation(anim);
     }
 
     @Override
-    public void onBackPressed()
-    {
-        Toast.makeText(getApplicationContext(), "Can not Cancel", Toast.LENGTH_LONG).show();
-    }
+    public void onBackPressed() {
 
+
+        if (exit) {
+            super.onBackPressed();
+            return;
+        }
+        this.exit = true;
+        Toast.makeText(this, "Click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                exit =false;
+            }
+        }, 2000);
+    }
 }
